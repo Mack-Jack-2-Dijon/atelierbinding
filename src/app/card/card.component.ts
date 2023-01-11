038e35083a13fd44c9f6ec50772e3c3413cb42a8
FIRST\_NAME LAST\_NAME <Mack-Jack-2-Dijon\jcqs.thm@gmail.com>
2023-01-11 08:44:12 +0100

    second

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -9,8 +9,30 @@ export class CardComponent {
   citation: string = '';
   age!:number;
   result!:number;
+  element = false;
+  user = {
+    name : 'Doe',
+    firstName : 'John',
+    age : 25,
+    quote : '',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+  result1:boolean = true;
 
+  sub(): void {
+    this.result1 = !this.result1;
+  }
   add(): void {
     this.result = this.age + 1;
   }
-}
+  
+    
+    
+    
+  
+    
+  
+  }
+
+  
+
